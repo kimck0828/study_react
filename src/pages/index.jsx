c476b80d1ac4358775351fdf4f5ccfcf97f3c05e
kimck0828 <kimck0828@gmail.com>
2023-04-24 16:25:09 +0900

    :sparkles: onClick、useEffect実装をやってみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,20 @@ import {Main} from "@/src/components/Main";
 import {Header} from "@/src/components/Header";
 
 export default function Index() {
+  function handleClick(e) {
+    console.log(e.target)
+    e.preventDefault();
+  }
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header/>
+      <a href={"/about"}
+         onClick={handleClick}>ボタン
+      </a>
       <Main page={"index"}/>
     </>
   )
