811e3811a1bf877d8263dcc02f96b6b4295f07f3
kimck0828 <kimck0828@gmail.com>
2023-04-24 18:34:22 +0900

    :sparkles: useStateで文字列、真偽値の例を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,23 +6,35 @@ import {useCallback, useEffect, useState} from "react";
 
 export default function Index() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [isShowCount, setShowCount] = useState(true);
 
   useEffect(() => {
-    console.log("useEffect", count)
+    // console.log("useEffect", count)
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("useEffect　アンマウント時", count)
+      // console.log("useEffect　アンマウント時", count)
       document.body.style.backgroundColor = "";
     }
   }, [count]);
-  
+
   const handleClick = useCallback((e) => {
     setCount(curr => curr + 1);
-    // setCount(count+1)
     e.preventDefault();
-  },[count]);
+  }, []);
 
+  const handleText = useCallback((e) => {
+    console.log(`text:${text}, e.target.value:${e.target.value} `)
+    if (e.target.value.length <= 5) {
+      setText(e.target.value);
+    }
+  }, []);
+
+  const handleShowCount = useCallback(() => {
+    setShowCount(curr => !curr)
+  }, [])
   console.log("レンダリング！")
+
   return (
     <>
       <Head>
@@ -30,10 +42,19 @@ export default function Index() {
       </Head>
       <Header/>
       <div className={style.index_center}>
-        <h1>{count}</h1><br/>
-        <button
-          onClick={handleClick}>ボタン
-        </button>
+        <h1>{isShowCount ? count : null}</h1>
+        <div>
+          <button
+            onClick={handleClick}>ボタン
+          </button>
+          &nbsp;&nbsp;
+          <button onClick={handleShowCount}>
+            {isShowCount ? "カウント非表示" : "カウント表示"}
+          </button>
+        </div>
+        <br/>
+        <input type={"text"} value={text} onChange={handleText}/>
+
       </div>
       <Main page={"index"}/>
     </>
