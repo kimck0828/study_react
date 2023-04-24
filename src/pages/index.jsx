3897a85f57f8536b2dcde5bdc55ca7895a16b158
kimck0828 <kimck0828@gmail.com>
2023-04-24 18:00:42 +0900

    :sparkles: useEffect,useCallbackの第二引数の実装

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,27 @@ import Head from 'next/head'
 import {Main} from "@/src/components/Main";
 import {Header} from "@/src/components/Header";
 import style from "@/src/styles/Home.module.css";
-import {useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 export default function Index() {
   const [count, setCount] = useState(0);
-  
-  console.log("レンダリング！")
-  function handleClick(e) {
-    setCount(curr => curr + 1);
-    e.preventDefault();
-  }
 
+  useEffect(() => {
+    console.log("useEffect", count)
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("useEffect　アンマウント時", count)
+      document.body.style.backgroundColor = "";
+    }
+  }, [count]);
+  
+  const handleClick = useCallback((e) => {
+    setCount(curr => curr + 1);
+    // setCount(count+1)
+    e.preventDefault();
+  },[count]);
+
+  console.log("レンダリング！")
   return (
     <>
       <Head>
